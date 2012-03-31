8dace244a369dac3ace00b279b4a401115a2d2f2
Trifon Trifonov <ttrifonov@asteasolutions.com>
2012-04-01 01:43:06 +0300

    Switched to using GNU make, using makefiles autogenerated from CDT.

---
+++ RegistrySettingsHandler/build/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Based on automatically-generated file by Eclipse CDT
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../RegistrySettingsHandler.cpp \
+../jsoncpp.cpp 
+
+OBJS += \
+./RegistrySettingsHandler.o \
+./jsoncpp.o 
+
+CPP_DEPS += \
+./RegistrySettingsHandler.d \
+./jsoncpp.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
